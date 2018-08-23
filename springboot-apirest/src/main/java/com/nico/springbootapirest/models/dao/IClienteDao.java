63350063f8ad80ac7b7cc4bd1699a4a26505a49e
Nico <pascualnicolass@gmail.com>
2018-08-22 22:45:35 -0300

    Delete IClienteDao.java

--- springboot-apirest/src/main/java/com/nico/springbootapirest/models/dao/IClienteDao.java
+++
@@ -1,8 +0,0 @@
-package com.nico.springbootapirest.models.dao;
-
-import com.nico.springbootapirest.models.entity.Cliente;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
-
-}
